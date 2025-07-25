c73ed5c278948fcdf8a3eb08040df7f98e5fde50
yash750 <yashjnv641@gmail.com>
2025-07-25 12:17:13 +0530

    Updates

--- yulu_ride/routes/db_connection.route.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import queryService from "../controllers/queryService.controller.js";
-
-const router = Router();
-
-router.post("/query", queryService);
-
-export default router;
--- yulu_ride/tests/setup.js
+++
@@ -1,8 +0,0 @@
-// This file sets up the test environment
-import { jest } from '@jest/globals';
-
-// Mock the database connection to use the test database
-jest.mock('../db/connectDatabase.js', () => {
-  const originalModule = jest.requireActual('../db/connectTestDB.js');
-  return originalModule;
-});
